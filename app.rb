b7332d3b8d71342af9f3ec31bb19a2f29a1a360b
akkireddyanusha <akkireddyanusha@gmail.com>
2018-08-01 19:04:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,33 +2,34 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
+  get '/' do
+    erb :index
+    
   end
 
   # Add your post route and action below
-post "/food" do 
-  @name=params[:name]
-  @favorite_food=params[:favorite_food]
-  erb :food
-  "My name is #{@name} and I love #{@favorite_food}"
-end 
-end
+# post "/food" do 
+#   @name=params[:name]
+#   @favorite_food=params[:favorite_food]
+#   erb :food
+#   "My name is #{@name} and I love #{@favorite_food}"
+# end 
+# end
 
-require_relative 'config/environment'
+# require_relative 'config/environment'
 
-class App < Sinatra::Base
+# class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
-  end
+#   get '/food_form' do
+#     erb :food_form
+#   end
 
-  # Add your post route and action below
-post "/food" do 
-  @top=params[:top]
-  @bottom=params[:bottom]
-  @footwear=params[:footwear]
-  erb :food
-  "Today you will wear a #{@top} with a #{@bottom} and #{@footwear}!:)"
-end 
-end
+#   # Add your post route and action below
+# post "/food" do 
+#   @top=params[:top]
+#   @bottom=params[:bottom]
+#   @footwear=params[:footwear]
+#   erb :food
+#   "Today you will wear a #{@top} with a #{@bottom} and #{@footwear}!:)"
+# end 
+# end
